3d9f9834995adec9a023aa3d58f61c3f9186af9b
Anastasia811625 <86957944+Anastasia811625@users.noreply.github.com>
2021-12-17 19:21:58 +0300

    rendering of the list of posts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import axios from 'axios';
+import {useEffect, useState} from 'react';
+
 function App() {
+  const [postList, setPostList] = useState([])
+
+  const getPosts = async () => {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+    setPostList(response.data)
+  }
+
+  useEffect(getPosts, [])
 
   return (
     <div className="App">
-
+      <ol>
+        {postList.map(post => {
+          return (<li key={`ple-${post.id}`}>{post.title}</li>)
+        })}
+      </ol>
     </div>
   );
 }
