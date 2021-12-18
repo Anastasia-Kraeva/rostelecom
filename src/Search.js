4d76dc8f603c4667249f33c47197003a66b03a25
Anastasia811625 <86957944+Anastasia811625@users.noreply.github.com>
2021-12-18 18:29:15 +0300

    add sorting

--- src/Search.js
+++ src/Search.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
-const Search = ({handleChangeInput}) => {
+const Search = ({handleChangeInput, handleChangeSelect, sortValue, sortingStatusOptions}) => {
 
   return (
     <div className={'filterBox'}>
       <input onInput={handleChangeInput}/>
+      <select value={sortValue} onChange={handleChangeSelect}>
+        {sortingStatusOptions.map(option => {
+          return (<option key={`fo-${option.id}`} value={option.value}>{option.value}</option>)
+        })}
+      </select>
     </div>
   );
 };
